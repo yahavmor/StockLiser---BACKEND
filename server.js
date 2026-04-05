5c1d8fe092bfd95738081a738ea370300f153919
yahavmor <yahavmor77@gmail.com>
2026-04-05 18:32:52 +0300

    updating dist and server

--- server.js
+++ server.js
@@ -7,18 +7,23 @@ import session from "express-session"
 import MongoStore from "connect-mongo"
 import { memeRoutes } from "./api/meme/meme.routes.js"
 import { stockRoutes } from "./api/stock/stock.routes.js"
-
+import path from "path"
+import { fileURLToPath } from "url"
 
 dotenv.config()
 
 const app = express()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 app.use(express.json())
 
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'https://backend-stock-sv6k.onrender.com'
   ],
   credentials: true
 }))
@@ -29,13 +34,13 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   store: MongoStore.create({
-    mongoUrl: process.env.MONGO_URI, 
+    mongoUrl: process.env.MONGO_URI,
     ttl: 60 * 60 * 24
   }),
   cookie: {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax"
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
   }
 }))
 
@@ -43,10 +48,15 @@ app.use('/api/auth', authRoutes)
 app.use('/api/meme', memeRoutes)
 app.use('/api/stock', stockRoutes)
 
+app.use(express.static(path.join(__dirname, "dist")))
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"))
+})
 
 await connectToDB()
 
-app.listen(3030, () => {
-  console.log("Server running on port 3030")
+const PORT = process.env.PORT || 3030
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT)
 })
